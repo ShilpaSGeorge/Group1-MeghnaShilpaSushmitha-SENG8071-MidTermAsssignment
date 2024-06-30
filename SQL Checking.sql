c3cb44bb0b941b9a418794906b4e59691d8c5c05
Shilpa Sosa George <shilpasosageorge@gmail.com>
2024-06-30 18:49:59 -0400

    SQL

--- SQL Checking.sql
+++ SQL Checking.sql
@@ -3,24 +3,24 @@ CREATE DATABASE Online BookStore Project;
 CREATE TABLE `Authors` (
   `Author_ID` INT,
   `Author_Name` VARCHAR(20),
-  `Book_ID` INT,
+  `DOB` DATE,
   `Book_Genre` VARCHAR(20),
-  `Date_Published` DATE,
-  PRIMARY KEY (`Author_ID`),
-  FOREIGN KEY (`Book_ID`) REFERENCES `Books`(`Book_ID`)
+  
+  PRIMARY KEY (`Author_ID`)
+  
 );
 
-INSERT INTO Authors VALUES(1001,'J K ROWLING',2001,'FANTASY',26 -06 -1997),(1002,'Peter Rob',2002,'COMPUTERS',03 -01 -2013),
-(1003,'Brothers Grimm',1918,'FAIRY TALE',12 -06 -1918),(1001,'J K ROWLING',2004,'FANTASY',02 -07 -1998), (1002,'Carlos',2005,'COMPUTERS',03 -01 -2013),
-(1001,'J K ROWLING',2006,'FICTION',26 -09 -2023) ;
-
+INSERT INTO Authors VALUES(1001,'J K ROWLING','26 -06 -1976','FANTASY'),
+(1002,'Peter Rob','03 -01 -1980','COMPUTERS'),
+(1003,'Brothers Grim','12-06-1780','FAIRY TALE'), 
+(1004,'Carlos','03 -01 -1950','COMPUTERS');
 
 SELECT * FROM Authors 
 WHERE Book_Genre = 'FANTASY';
 
 
 UPDATE Authors 
-SET Book_ID = 2003
+SET Author_Name = 'Brothers Grimm'
 WHERE Author_ID = 1003;
 
 
@@ -33,14 +33,19 @@ CREATE TABLE `Books` (
   `Book_Name` VARCHAR(50),
   `Book_Genre` VARCHAR(20),
   `Author_ID` INT,
+  `Date_Published` DATE,
   `Price` DECIMAL,
   `Stock` INT,
+  `Book_Type` VARCHAR(20),
   PRIMARY KEY (`Book_ID`),
   FOREIGN KEY (`Author_ID`) REFERENCES `Authors`(`Author_ID`)
 );
 
-INSERT INTO Books VALUES(2001, 'Harry Potter Part 1','FANTASY',1001, 230.56, 10),(2002, 'Database Systems','COMPUTERS',1002, 330.56, 15), 
-(2003, 'Snow White','FAIRY TALE',1003, 200.00, 20), (2004, 'Harry Potter Part 2','FANTASY',1001, 230.56, 12), (2006, 'The Running Grave','FANTASY',1001, 280.56, 15) ;
+INSERT INTO Books VALUES(2001, 'Harry Potter Part 1','FANTASY',1001,'26-07-1997', 230.56, 10, 'e-book'),
+(2002, 'Database Systems','COMPUTERS',1002, '12-03-1998', 330.56, 15, 'physical book'), 
+(2003, 'Snow White','FAIRY TALE',1003,'27-08-1918', 200.00, 20, 'audiobook'),
+(2004, 'Harry Potter Part 2','FANTASY',1001,'29-06-1998', 230.56, 12, 'e-book'), 
+(2006, 'The Running Grave','FANTASY',1001, '12-01-2023', 280.56, 15, 'audiobook') ;
 
 SELECT A.Author_ID,
 A.Author_Name
@@ -52,4 +57,26 @@ WHERE A.Book_Genre = B.Book_Genre
 AND A.Date_Published >= (YEAR(CURRENT_DATE) - 30)
 GROUP BY A.Author_ID
 HAVING COUNT (DISTINCT B.Book_ID)>2;
+
+SELECT Customer_ID, SUM(Sale_Price) AS Total_Spending
+FROM Sales
+WHERE Date_sold >= DATEADD(YEAR, -1, CURRENT_DATE)
+GROUP BY Customer_ID
+HAVING SUM(Sale_Price) > 1000;
+
+SELECT B.Book_ID, B.Book_Title
+FROM Books AS B
+JOIN Reviews AS R ON B.Book_ID = R.Book_ID
+GROUP BY B.Book_ID, B.Book_Title
+HAVING AVG(R.Rating_level) > (SELECT AVG(Rating_level) FROM Reviews);
+
+SELECT Book_Genre, SUM(Sale_Price) AS Total_Sales
+FROM Sales
+GROUP BY Book_Genre
+ORDER BY Total_Sales DESC
+LIMIT 1;
+
+SELECT * from Reviews
+ORDER BY Review_Published DESC
+LIMIT 10;
 
